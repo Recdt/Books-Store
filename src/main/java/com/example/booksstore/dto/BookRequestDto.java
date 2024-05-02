5eabba43c5681332365c3c89673a17e96344476c
Chopyk Mykola <record7654@gmail.com>
2024-05-02 17:47:44 +0300

    Some improvements

--- src/main/java/com/example/booksstore/dto/BookRequestDto.java
+++ src/main/java/com/example/booksstore/dto/BookRequestDto.java
@@ -1,22 +1,23 @@
 package com.example.booksstore.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     @ISBN
     private String isbn;
     @NotNull
-    @Min(0)
+    @PositiveOrZero
     private BigDecimal price;
     private String description;
     private String coverImage;
